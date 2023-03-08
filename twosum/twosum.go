6c9fbb3383b7af37973ddda2cc35b3c84d190de6
justdave@linux.com <justdave@linux.com>
2023-03-08 10:44:29 -0600

    added brute force attempt for twosum

--- twosum/twosum.go
+++ twosum/twosum.go
@@ -1,5 +1,8 @@
 package twosum
 
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
 func TwoSum(nums []int, target int) []int {
 	out := []int{}
 	visited := map[int]int{} // Space O(n)
@@ -13,4 +16,28 @@ func TwoSum(nums []int, target int) []int {
 		visited[j] = i
 	}
 	return out
+}
+
+func TwoSumBruteForce(nums []int, target int) []int {
+    out := []int{}
+    // range over num slice
+    for i, j := range nums {
+        for x, y := range nums {
+            // check if output slice is less than 2
+            if len(out) < 2{
+                // check if same element
+                if i == x {
+                    continue
+                }
+                // check if sum equals target
+                sum := j + y
+                if sum == target {
+                    out = append(out, i)
+                    out = append(out, x)
+                    break
+                }
+            }
+        }
+    }
+    return out
 }
--- twosum/twosum_test.go
+++ twosum/twosum_test.go
@@ -18,12 +18,36 @@ func TestTwoSum(t *testing.T) {
 		// TODO: Add test cases.
 		{name: "Case1", args: args{nums: []int{2, 7, 11, 15}, target: 9}, want: []int{1, 0}},
 		{name: "Case2", args: args{nums: []int{3, 2, 4}, target: 6}, want: []int{2, 1}},
-		{name: "Case3", args: args{nums: []int{3,3}, target: 6}, want: []int{1, 0}},
+		{name: "Case3", args: args{nums: []int{3, 3}, target: 6}, want: []int{1, 0}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := TwoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("TwoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTwoSumBruteForce(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{name: "Case1", args: args{nums: []int{2, 7, 11, 15}, target: 9}, want: []int{0, 1}},
+		{name: "Case2", args: args{nums: []int{3, 2, 4}, target: 6}, want: []int{1, 2}},
+		{name: "Case3", args: args{nums: []int{3, 3}, target: 6}, want: []int{0, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TwoSumBruteForce(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TwoSumBruteForce() = %v, want %v", got, tt.want)
 			}
 		})
 	}
